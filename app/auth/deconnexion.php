6a1fffff09b19bb68afe0c29b19d04bfbaa1dc4a
Zina aloui <zinaaloui968@gmail.com>
2026-04-29 19:42:48 +0200

    Update deconnexion.php

--- app/auth/deconnexion.php
+++ app/auth/deconnexion.php
@@ -1,5 +1,20 @@
 <?php
 session_start();
+require_once 'db.php';
+
+// Supprimer le cookie remember me
+if (isset($_COOKIE['remember_token'])) {
+    $token = $_COOKIE['remember_token'];
+    $pdo = Database::getConnection();
+    $pdo->prepare("DELETE FROM user_tokens WHERE token = ?")->execute([$token]);
+    setcookie('remember_token', '', time() - 3600, '/');
+}
+
+// Détruire la session
+$_SESSION = array();
 session_destroy();
-header('Location: ../index.php?logout=1');
+
+// Rediriger vers login
+header('Location: login.php');
 exit;
+?>
